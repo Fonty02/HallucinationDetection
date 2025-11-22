b8846cb1fc8d99e04c70ccb4d9143a66ad44a945
Fontana <fontanaemanuele14@gmail.com>
2025-11-22 22:08:45 +0100

    Ex

--- src/model/prompts.py
+++ src/model/prompts.py
@@ -1,20 +1,8 @@
-PROMPT_QA = """You are an helpful assistant that replies to some questions. 
-You will also provided with the answer given by another agent. Don't use it while replying.
+PROMPT_QA = """Answer the following question with just the essential information, without explanations.
 
 Question: {question}
 
-Other agent answer: {answer}
-
-Your answer:"""
+Answer:"""
 
 
-PROMPT_CORRECT = """You are an helpful assistant that helps to identify hallucinations.
 
-Given a question and an answer provided by another agent, you have to determine if the answer is correct or not.
-Reply with "yes" if the answer is correct, otherwise reply with "no".
-
-Question: {question}
-
-Other agent answer: {answer}
-
-Is the answer correct? (yes/no)"""
