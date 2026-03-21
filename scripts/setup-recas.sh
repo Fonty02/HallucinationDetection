1eddb0015bb3c2c263e6acb7e87b540be98ad6f2
Emanuele Fontana <fonty@ui03.recas.ba.infn.it>
2026-03-21 10:20:02 +0100

    Extraction done

--- scripts/setup-recas.sh
+++ scripts/setup-recas.sh
@@ -11,8 +11,7 @@ source .venv/bin/activate
 # rm -rf ~/.cache/python
 # 
 
-
+export HF_TOKEN="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"
 hf download google/gemma-2-9b-it
-hf download Qwen/Qwen2.5-7B
 hf download meta-llama/Llama-3.1-8B-Instruct
-hf download tiiuae/Falcon3-7B-Base
+
